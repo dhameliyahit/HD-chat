de0a36e37d931b3a762d3ab4ab934b9717bad6ad
dhameliyahit <heetdhameliya59@gmail.com>
2024-09-10 22:09:36 +0530

    add: imrove styling

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIo(server);
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+'/public'+'/index.html');
-})
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
-io.on("connection",(socket)=>{
-    console.log("User connect");
-
-    socket.on('chat message',(msg)=>{
-        io.emit('chat message',msg);
-    })
-
-    socket.on("disconnect",()=>{
-        console.log("User disconnected");
-    })
+// Serve the HTML file when the root URL is accessed
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
+// Handle socket connections
+io.on('connection', (socket) => {
+    console.log('A user connected');
 
+    // Handle chat messages
+    socket.on('chat message', (data) => {
+        const { userName, msg } = data;
+        io.emit('chat message', { userName, msg }); // Broadcast the message with userName to all clients
+    });
 
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
-server.listen(3000,()=>{
-    console.log(`Server is Running on PORT no. 3000`);
-})
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
